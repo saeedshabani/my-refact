710929fe4b6c89191f05fe38355487eecc92d146
saeed shabani <saeedshabani@users.noreply.github.com>
2025-03-19 08:14:48 +0330

    Feat: update eslint config and install flickity for sliders

--- wp-content/themes/refact-starter/.eslintrc.js
+++ wp-content/themes/refact-starter/.eslintrc.js
@@ -5,17 +5,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended'
+    'eslint:recommended',
+    // Add 'plugin:import/errors' if you're using the import plugin  
+    'plugin:import/errors',  
+    'plugin:import/warnings',  
+    'plugin:import/typescript'  
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'script',
+    // sourceType: 'script',
+    sourceType: 'module',  // Allow using import/export statements  
   },
   rules: {
     "no-console": "off", // Allow console.log for debugging
     "prefer-arrow-callback": "error", // Enforce arrow functions for callbacks
     "func-names": "off", // Disable requiring function names
-    "strict": ["error", "global"], // Enforce strict mode globally
+    'strict': 'off', // Disable the strict rule  
+    // "strict": ["error", "global"], // Enforce strict mode globally
   },
   globals: {
     document: 'readonly',
